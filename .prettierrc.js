a89177d08242dad17980b10bd6a5fc9aeca074ea
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2020-10-11 16:22:15 -0300

    .prettierrc.js: Setupping lint

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    "singleQuote": true,
+    "trailingComma": "all",
+    "useTabs": true,
+    "tabWidth": 2,
+    "printWidth": 100,
+    "overrides": [
+      {
+        "files": "*.js",
+      }
+    ]
+  };
